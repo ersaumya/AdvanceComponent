38267dccb2de12a4e503cf8e114e7858f12ebb8d
ersaumya <ersaumyarout@gmail.com>
2020-03-01 17:37:47 +0530

    dynamic component added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { AuthFormComponent } from './auth-form/auth-form.component';
+import { Component, ViewChild, ViewContainerRef, ComponentFactoryResolver, AfterContentInit, ComponentRef, TemplateRef } from '@angular/core';
 import { User } from './auth-form/auth-form.interface';
 
 @Component({
@@ -6,20 +7,50 @@ import { User } from './auth-form/auth-form.interface';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-
-  rememberMe: boolean;
-
-  rememberUser(remember: boolean) {
-    this.rememberMe = remember;
-  }
-
-  createUser(user: User) {
-    console.log('create account', user);
+export class AppComponent implements AfterContentInit{
+ 
+  component:ComponentRef<AuthFormComponent>;
+ 
+  constructor(private resolver:ComponentFactoryResolver){
 
   }
+  ngAfterContentInit() {
+    setTimeout(() => {
+    //const authFormFactory=this.resolver.resolveComponentFactory(AuthFormComponent);
+    //second component 
+    ///this.entry.createComponent(authFormFactory);
+    //this.component=this.entry.createComponent(authFormFactory,0); /* 0-indiacte reordering of component to first */
+    /* dynamic input data */
+    /* this.component.instance.title="Create Account"; */
+    /*   dynamic output subscription */
+    /* this.component.instance.submitted.subscribe(this.LoginUser);
+    })  */
+
+    //embeded template
+    this.entry.createEmbeddedView(this.tmpl,{
+      $implicit:'Soumya',
+      location:'Salipur,India'
+    });
+  })
+}
+
+  /* logic for dynamic component */
+  @ViewChild('entry',{read:ViewContainerRef}) entry:ViewContainerRef;
+
+  @ViewChild('tmpl') tmpl:TemplateRef<any>;
+
+ 
+  //destroy dynamic component
+  destroyComponent(){
+    this.component.destroy();
+  }
+  //reordering component
+  moveComponent() {
+    this.entry.move(this.component.hostView, 1);
+  }
+
   LoginUser(user: User) {
-    console.log('login user', user, this.rememberMe);
+    console.log('login user', user);
   }
 
 }
